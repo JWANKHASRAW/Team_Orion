cdc917e060a55eb3f4ce24f9c86fd3bd02ffbe58
Jwan Khasraw <jwan_khasraw@yahoo.com>
2023-05-22 10:29:32 -0700

    Created OddAndEvenNumber.java

---
+++ src/Jwan/OddAndEvenNumber.java
@@ -0,0 +1,23 @@
+package Jwan;
+
+public class OddAndEvenNumber {
+    public static void main(String[] args) {
+
+        OddOrEven(6);
+    }
+
+    public static void OddOrEven(int number){
+
+
+        if(number %2==0){
+            System.out.println(number + " is an even number");
+
+        }else{
+            System.out.println(number + " is an odd number");
+
+        }
+
+
+    }
+
+}
