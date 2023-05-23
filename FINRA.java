732f7644c3bf6c5a4062eba8b830e479a3d81630
djamelia <djameliaturcanu@gmail.com>
2023-05-23 10:50:17 -0700

    Created the FINRA method

---
+++ FINRA.java
@@ -0,0 +1,35 @@
+public class FINRA {
+
+
+    public static void FINRA() {
+
+        for (int i = 0; i < 30; i++) {
+            boolean by3 = i % 3 == 0;
+            boolean by5 = i % 5 == 0;
+            if (by3 && by5) {
+                System.out.println("FINRA");
+            } else if (by3) {
+                System.out.println("FIN");
+            } else if (by5) {
+                System.out.println("RA");
+            } else {
+                System.out.println(i);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+
+        FINRA();
+
+    }
+}
+
+
+
+
+
+
+
