aae71bf94e08307475280d2e4e94398b5450c988
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-05-23 12:48:09 -0700

    Division

--- Anas/DivideWithoutOperator.java
+++ Anas/DivideWithoutOperator.java
@@ -2,17 +2,21 @@ package Anas;
 
 public class DivideWithoutOperator {
     public static void divider(int dividend,int divisor){
-       int iterations=0;
-       int division=dividend-divisor;
-        for (int i = division; divisor>0; i++) {
-            division = dividend-divisor;
-            iterations++;
+        int result=0;
+        if(dividend-divisor>0){
+            while (dividend>=divisor){
+                dividend=dividend-divisor;
+                result++;
+            }
+            System.out.println(result);
+        }else {
+            System.err.println("Number cannot be negative");
         }
-        System.out.println(iterations);
+
     }
 
     public static void main(String[] args) {
-        divider(10,5);
+        divider(20,3);
     }
     //NOT solved yet
 }
