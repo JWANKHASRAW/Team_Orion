3871abfad78dc9d5f4ec73777ffd6dbcc58ea3ab
cattyray <catty_ray@yahoo.com>
2023-05-22 21:11:25 -0700

    Updated MethodWithoutOperator class

--- ThreeMethods/Finra.java
+++ ThreeMethods/Finra.java
@@ -3,18 +3,18 @@ package ThreeMethods;
 public class Finra {
 
     public static void main(String[] args) {
-        int a= 6;
-        int b= 15;
+        int a = 6;
+        int b = 15;
 
-        for(int i = 0; i<30; i = i+3){
-            if(i%3==0){
+        for (int i = 0; i < 30; i = i + 3) {
+            if (i % 3 == 0) {
                 System.out.println("FIN");
             }
-            for(i = 1; i<30; i++){
-                if(i%5==0){
+            for (i = 1; i < 30; i++) {
+                if (i % 5 == 0) {
                     System.out.println("RA");
                 }
-                if(i%3==0 && i%5==0){
+                if (i % 3 == 0 && i % 5 == 0) {
                     System.out.println("FINRA");
                 }
             }
