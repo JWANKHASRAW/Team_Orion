0a0777740eff73f397bda7531d4c1f3bfaa77e54
djamelia <djameliaturcanu@gmail.com>
2023-05-23 14:48:30 -0700

    edited

--- week1/FINRA.java
+++ week1/FINRA.java
@@ -8,12 +8,13 @@ public class FINRA {
         for (int i = 0; i < 30; i++) {
             boolean by3 = i % 3 == 0;
             boolean by5 = i % 5 == 0;
+
             if (by3 && by5) {
-                System.out.println("FINRA");
+                System.out.println(i+" FINRA");
             } else if (by3) {
-                System.out.println("FIN");
+                System.out.println(i+" FIN");
             } else if (by5) {
-                System.out.println("RA");
+                System.out.println(i+" RA");
             } else {
                 System.out.println(i);
             }
