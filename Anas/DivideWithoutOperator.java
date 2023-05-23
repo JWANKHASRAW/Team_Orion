c38d155c61253d23cf8141ef1eb4807267a3b50c
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-05-23 11:45:38 -0700

    Division method (unsolved)

---
+++ Anas/DivideWithoutOperator.java
@@ -0,0 +1,18 @@
+package Anas;
+
+public class DivideWithoutOperator {
+    public static void divider(int dividend,int divisor){
+       int iterations=0;
+       int division=dividend-divisor;
+        for (int i = division; divisor>0; i++) {
+            division = dividend-divisor;
+            iterations++;
+        }
+        System.out.println(iterations);
+    }
+
+    public static void main(String[] args) {
+        divider(10,5);
+    }
+    //NOT solved yet
+}
