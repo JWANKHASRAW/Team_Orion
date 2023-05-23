4d4f3204514f0e95c272c897ebfa1c4f03d551c0
cattyray <catty_ray@yahoo.com>
2023-05-23 12:11:07 -0700

    Updated Finra class

--- ThreeMethods/Finra.java
+++ ThreeMethods/Finra.java
@@ -3,14 +3,12 @@ package ThreeMethods;
 public class Finra {
 
     public static void main(String[] args) {
-        int a = 6;
-        int b = 15;
 
-        for (int i = 0; i < 30; i = i + 3) {
+
+        for (int i = 0; i < 30; i++) {
             if (i % 3 == 0) {
                 System.out.println("FIN");
             }
-            for (i = 1; i < 30; i++) {
                 if (i % 5 == 0) {
                     System.out.println("RA");
                 }
@@ -20,4 +18,4 @@ public class Finra {
             }
         }
     }
-}
+
