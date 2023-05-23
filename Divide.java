aafec1e97b7c316f01422565c0d8af28803e66de
djamelia <djameliaturcanu@gmail.com>
2023-05-23 11:16:03 -0700

    Created divide method

--- Divide.java
+++ Divide.java
@@ -3,7 +3,7 @@ public class Divide {
 public static int divide(int num1,int num2){
 
     int count=0;
-    if (num1 > 0 && num2>0){
+    if (num1 > 0 && num2 > 0){
         while (num1>=num2){
             num1=num1-num2;
             count++;
