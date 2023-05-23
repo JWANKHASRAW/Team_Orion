edd9f25d241da5e822840968fa71fae31229d77f
thaladch <moggrand@gmail.com>
2023-05-23 13:51:59 -0400

    created OddOrEven number

---
+++ OddOrEven.java
@@ -0,0 +1,14 @@
+public class OddOrEven {
+
+    public static void main(String[] args) {
+
+        int num = 26;
+
+        if (num % 2 == 0){
+            System.out.println(num+ " is an even number");
+        }else {
+            System.out.println(num+ " is an odd number");
+        }
+
+    }
+}
