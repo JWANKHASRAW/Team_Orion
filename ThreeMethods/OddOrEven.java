1bb537e05037bffc3341221acfd35a130e427899
cattyray <catty_ray@yahoo.com>
2023-05-22 17:21:37 -0700

    Created OddOrEven class

---
+++ ThreeMethods/OddOrEven.java
@@ -0,0 +1,14 @@
+package ThreeMethods;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+
+        int number = 5;
+        boolean isEven = number % 2 == 0;
+        boolean isOdd = !isEven;
+
+        System.out.println(number + " is even number: " + isEven);
+        System.out.println(number + " is odd number: " + isOdd);
+    }
+}
+
