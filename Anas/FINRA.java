b582632815a6f6dfba5a5a48a23f7431584e114f
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-05-23 12:13:34 -0700

    FINRA UPDATE

--- Anas/FINRA.java
+++ Anas/FINRA.java
@@ -1,22 +1,15 @@
 package Anas;
 
 public class FINRA {
-    public static void finra(){
+    public static void Finra(){
         for (int i = 1; i <=30 ; i++) {
-            if(i%3==0){
-                System.out.println("FIN");
-            }
-            if(i%5==0){
-                System.out.println("RA");
-            }
-            if(i%3==0&&i%5==0){
-                System.out.println("FINRA");
-            }
-            System.out.println(i);
+                System.out.println(
+                        (i%3==0)? "FIN" :(i%5==0)?"RA":(i%3==0&&i%5==0)?"FINRA" : i
+                );
         }
     }
 
     public static void main(String[] args) {
-        finra();
+        Finra();
     }
 }
