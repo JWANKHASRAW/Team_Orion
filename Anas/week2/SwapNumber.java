e63bb19fd07b85723d06c3d8893757244f39b4fb
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-05-26 14:43:13 -0700

    week2

--- Anas/week2/SwapNumber.java
+++ Anas/week2/SwapNumber.java
@@ -1,4 +1,10 @@
 package Anas.week2;
 
 public class SwapNumber {
+    //Numbers -- Swap Numbers
+    //Swap two variables values without using a third variable
+
+    public static void main(String[] args) {
+
+    }
 }
