f91e776355307e5ba149471101eccb373a333ec6
Djamelia <djameliaturcanu@gmail.com>
2023-05-27 14:45:14 -0700

    created Consecutive class

---
+++ week2/ConsecutiveNums.java
@@ -0,0 +1,28 @@
+package week2;
+
+public class ConsecutiveNums {
+    public static void main(String[] args) {
+
+
+        int num = 24;
+
+        for (int i = 1; i <= num; i++) {
+
+            if (i % 2 == 0 ){
+                System.out.println("Codility");
+            } else if(i % 2 == 0 && i % 3 == 0 ){
+                System.out.println("CodilityTest");
+            } else if (i % 3 == 0 ) {
+                System.out.println("Test");
+            }else if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
+                System.out.println("CodilityTestCoders");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            }else {
+                System.out.println(i);
+            }
+        }
+
+
+    }
+}
