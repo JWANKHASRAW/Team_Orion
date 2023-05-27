5a895190dc18050100c1266067feb9894a71527e
Djamelia <djameliaturcanu@gmail.com>
2023-05-27 14:38:36 -0700

    created Swap class

---
+++ week2/SwapNums.java
@@ -0,0 +1,24 @@
+package week2;
+
+public class SwapNums {
+    public static void main(String[] args) {
+
+        public static void main(String[] args) {
+
+            int x = 10;
+            int y = 20;
+
+            x = x + y;  // 10+20=30
+            y = x - y;  // 30-20=10
+            x = x - y;  // 30-10=20
+
+            System.out.println(" x = " + x + "\n y = " + y);
+
+
+
+
+        }
+
+
+    }
+}
