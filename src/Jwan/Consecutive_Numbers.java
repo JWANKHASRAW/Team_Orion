c4e1eb92d3f876e51e3d0383fb462f881832a92d
Jwan Khasraw <jwan_khasraw@yahoo.com>
2023-05-27 21:31:59 -0700

    Created Consecutive_Numbers.java

---
+++ src/Jwan/Consecutive_Numbers.java
@@ -0,0 +1,35 @@
+package Jwan;
+
+public class Consecutive_Numbers {
+    public static void main(String[] args) {
+
+        int num1=30;
+
+        for (int i = 1; i < num1; i++) {
+
+            if (i %2 ==0 && i %3 ==0 && i%5==0){
+                System.out.println("CodilityTestCoders");
+            }else if (i%2==0 && i %3 ==0){
+                System.out.println("CodilityTest");
+            } else if (i %2 ==0 && i%5 ==0){
+                System.out.println("CodilityCoders");
+            }else if (i%3 ==0 && i%5==0){
+                System.out.println("TestCoders");
+            }else if(i%2 ==0){
+                System.out.println("Codility");
+            }else if (i%3==0){
+                System.out.println("Test");
+            }else if(i%5==0){
+                System.out.println("Coders");
+            }else{
+                System.out.println(i);
+            }
+
+        }
+
+
+
+
+
+    }
+}
