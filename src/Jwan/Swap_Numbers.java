1157c9c704fccf495b15ba28eaed5e312c8f0ee4
Jwan Khasraw <jwan_khasraw@yahoo.com>
2023-05-27 21:25:44 -0700

    Created Swap_Numbers.java

---
+++ src/Jwan/Swap_Numbers.java
@@ -0,0 +1,21 @@
+package Jwan;
+
+public class Swap_Numbers {
+    public static void main(String[] args) {
+
+
+        double num1 = 4.5;
+        double num2 = -2;
+
+        num1 = num1*num2; // 1*2= 2
+        num2 = num1/num2; // 2/2=1
+        System.out.println("num2 = " + num2 );
+        num1 = num1/num2; // 2/1 = 2
+        System.out.println("num1 = " + num1 );
+
+
+
+
+
+    }
+}
