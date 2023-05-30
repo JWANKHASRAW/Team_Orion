1dc45428a764f991e9f75825586e139dc9e914bf
SORINA <sorina.zdragus@yahoo.com>
2023-05-29 17:23:37 -0700

    Created test class

---
+++ Test.java
@@ -0,0 +1,14 @@
+public class Test {
+
+    public static void main(String[] args) {
+
+        int number = 5;
+
+        boolean isEven = number % 2 == 0;
+        boolean isOdd = !isEven;
+        System.out.println(number + " is even number: " + isEven );
+        System.out.println(number + " is odd number: " + isOdd );
+
+
+    }
+}
