7e4ba7b7047a54c7e72454d966b709cd6f0fc8e2
Djamelia <djameliaturcanu@gmail.com>
2023-05-30 14:59:55 -0700

    edited

--- week2/ConsecutiveNums.java
+++ week2/ConsecutiveNums.java
@@ -5,19 +5,19 @@ public class ConsecutiveNums {
 
 
         int num = 24;
+        String by2 = "Codility", by3 = "Test", by5 = "Coders";
 
         for (int i = 1; i <= num; i++) {
-
-            if (i % 2 == 0 ){
-                System.out.println("Codility");
+            if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0) {
+                System.out.println(by2 + by3 + by5);
             } else if(i % 2 == 0 && i % 3 == 0 ){
-                System.out.println("CodilityTest");
-            } else if (i % 3 == 0 ) {
-                System.out.println("Test");
-            }else if (i % 2 == 0 && i % 3 == 0 && i % 5 == 0){
-                System.out.println("CodilityTestCoders");
+                System.out.println(by2 + by3);
             } else if (i % 5 == 0) {
-                System.out.println("Coders");
+                System.out.println(by5);
+            } else if (i % 3 == 0 ) {
+                System.out.println(by3);
+            }else if (i % 2 == 0 ){
+                System.out.println(by2);
             }else {
                 System.out.println(i);
             }
