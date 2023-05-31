12d526a51293edf29deb450d85c28846cac3b890
cattyray <catty_ray@yahoo.com>
2023-05-31 11:38:46 -0700

    updated SwapValues

--- ThreeMethods/SwapValues.java
+++ ThreeMethods/SwapValues.java
@@ -4,14 +4,14 @@ public class SwapValues {
 
     public static void main(String[] args) {
         int a = 10;
-        int b= 20;
-        System.out.println("a= "+a);
-        System.out.println("b= "+b);
+        int b = 20;
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
 
-        a = a+b;
-        b= a-b;
-        a= a-b;
-        System.out.println("a= " +a);
-        System.out.println("b= " +b);
+        a = a + b; //30
+        b = a - b; //30-10 =20
+        a = a - b; //30-20=10
+        System.out.println("a= " + a);
+        System.out.println("b= " + b);
     }
 }
