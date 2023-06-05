5a3e4e283c83ced27c5ff9edf6625581720a5219
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-06-05 16:09:07 -0700

    week3

--- Anas/week3/ReverseNegativeNumber.java
+++ Anas/week3/ReverseNegativeNumber.java
@@ -1,29 +1,13 @@
 package Anas.week3;
 
 public class ReverseNegativeNumber {
-    //Write a return method that can reveres negative number and return it as int
-
-    public static int Reverser(int num){
-        String result ="";
-        if(num%10==0){
-            num=num-1;
-            System.out.println("first digit looks like one but it's a zero, trust me");
-
-        }
-        String number = Integer.toString(num);
-        number=number.replace("-","");
-
-            for (int i = number.length()-1; i >= 0; i--) {
-                result+= number.charAt(i);
-            }
-
-        num= Integer.parseInt(result);
-        System.out.println(num);
-        return (num);
+    public static int Reverser2(int num){
+        int numRev = Math.abs(num);
+        System.out.println(numRev);
+        return numRev;
     }
 
     public static void main(String[] args) {
-        Reverser(-800000);
+        Reverser2(-80000000);
     }
-
 }
--- Anas/week3/ReverseNegativeNumber2.java
+++ Anas/week3/ReverseNegativeNumber2.java
@@ -1,13 +1,18 @@
 package Anas.week3;
 
 public class ReverseNegativeNumber2 {
-    public static int Reverser2(int num){
-        int numRev = Math.abs(num);
-        System.out.println(numRev);
-        return numRev;
+    public static int Reverser3(int num){
+
+        if(num<0){
+            num=-num;
+        }else {
+            num=num;
+        }
+        System.out.println(num);
+        return num;
     }
 
     public static void main(String[] args) {
-        Reverser2(-80000000);
+        Reverser3(-80000000);
     }
 }
