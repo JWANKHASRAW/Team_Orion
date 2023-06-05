14cc546e54cec45e1511a8fdbe73bcd2bb1452f0
Jwan Khasraw <jwan_khasraw@yahoo.com>
2023-06-04 20:13:01 -0700

    Updated PrimeNumber.java

--- src/Jwan/week3/PrimeNumber.java
+++ src/Jwan/week3/PrimeNumber.java
@@ -4,26 +4,22 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
+
         System.out.println("Please enter a number");
         Scanner input = new Scanner(System.in);
         int number = input.nextInt();
 
         boolean isPrime = true;
-        for (int i = 2; i <=number ; i++) {
-            if(number% i ==0){
+        int i =2;
+        while(i <= number/2){  //
+            if(number % i ==0){
                 isPrime = false;
-                System.out.println(number + " is not a prime number");
                 break;
             }
-            if(isPrime){
-                System.out.println(number + " is a prime number");
-                break;
-            }
-
-
+            i++;
         }
 
-
+        System.out.println(isPrime);
 
 
     }
