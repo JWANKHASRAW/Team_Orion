758ee1677aa7af0809b77524fac21556a19a4419
cattyray <catty_ray@yahoo.com>
2023-06-05 23:04:10 -0700

    Created Week3

---
+++ ThreeMethods/Week3/ReverseNegativeNumber.java
@@ -0,0 +1,26 @@
+package ThreeMethods.Week3;
+
+
+public class ReverseNegativeNumber {
+
+    public static void main(String[] args) {
+
+        // num1 * (-1)
+
+        int neg = -6;
+        int pos = neg * (-1);
+
+        System.out.println(neg + " is negative number");
+        System.out.println(pos + " is positive number");
+
+
+
+
+
+ /*
+  Numbers -- Reverse negative number
+Write a return method that can reverse negative number and return it as int
+  */
+    }
+
+}
