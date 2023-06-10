4000f8330df77fbe28cdf860e3da6761df38dad4
thaladch <moggrand@gmail.com>
2023-06-10 15:21:59 -0400

    created Consecutive Number

---
+++ Mai/ConsecutiveNumbers.java
@@ -0,0 +1,29 @@
+package Mai;
+
+public class ConsecutiveNumbers {
+
+    public static void main(String[] args) {
+
+        int num = 24;
+
+        for (int i = 1; i <= num ; i++) {
+                if(i%2 == 0 && i%3 == 0 && i%5 == 0 ){
+                    System.out.println("CodilityTestCoders");
+                } else if (i%2 == 0 && i%3 == 0){
+                    System.out.println("CodilityTest");
+                } else if (i%2==0 && i%5 ==0) {
+                    System.out.println("CodilityCoders");
+                } else if (i%3==0 && i%5== 0 ) {
+                    System.out.println("TestCoder");
+                } else if(i%2==0){
+                    System.out.println("Codility");
+                } else if(i%3==0){
+                    System.out.println("Test");
+                } else if (i%5 == 0 ){
+                    System.out.println("Coders");
+                } else {
+                System.out.println(i);
+            }
+        }
+    }
+}
