c2990066e625a65bdbaa459a99f01ba376b63f42
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-06-10 15:23:32 -0700

    week4

---
+++ Anas/week4/FrequencyOfCharacters.java
@@ -0,0 +1,10 @@
+package Anas.week4;
+
+public class FrequencyOfCharacters {
+    public static void main(String[] args) {
+//        String -- Frequency of Characters
+//        Write a return method that can find the frequency of
+//        characters
+//        Ex: FrequencyOfChars("AAABBCDD") ==> A3B2C1D2
+    }
+}
---
+++ Anas/week4/RemoveDuplicates.java
@@ -0,0 +1,10 @@
+package Anas.week4;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+//        String -- Remove Duplicates
+//        Write a return method that can remove the duplicated values from
+//                String
+//        Ex: removeDup("AAABBBCCC") ==> ABC
+    }
+}
