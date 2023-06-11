6b226852e0678538d481a8a9d60f3fefa695b578
Erden_Cydeo <erdenusdemirkol@gmail.com>
2023-06-11 15:47:10 -0700

    created SameLetters class

---
+++ week3/SameLetters.java
@@ -0,0 +1,23 @@
+package week3;
+
+import java.util.Arrays;
+
+public class SameLetters { public static boolean ifSameLetter(String a, String b) {
+    if (a == null) {
+        return b == null;
+    } else if (b == null) {
+        System.err.println("Given strings can not be null!");
+        System.exit(1);
+    }
+    char[] left = a.toLowerCase().toCharArray(); // [a, b, c ]
+    char[] right = b.toLowerCase().toCharArray(); // [b, c, a ]
+    Arrays.sort(left); // -- > [a, b, c]
+    Arrays.sort(right); // -- > [a, b ,c ]
+    return Arrays.equals(left, right); //==> if they are same returns true otherwise false
+}
+
+
+    public static void main(String[] args) {
+        System.out.println(ifSameLetter("acdfgh","hgdfcA"));
+    }
+}
