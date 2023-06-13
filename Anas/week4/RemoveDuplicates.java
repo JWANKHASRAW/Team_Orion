4bdede072276635f6f6a9bfb460ab96f1c042d6a
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-06-13 12:04:00 -0700

    week4

--- Anas/week4/RemoveDuplicates.java
+++ Anas/week4/RemoveDuplicates.java
@@ -1,10 +1,41 @@
 package Anas.week4;
 
+import java.util.*;
+
 public class RemoveDuplicates {
     public static void main(String[] args) {
 //        String -- Remove Duplicates
 //        Write a return method that can remove the duplicated values from
 //                String
 //        Ex: removeDup("AAABBBCCC") ==> ABC
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        String str = input.next();
+
+        /**Using String:*/
+//        String dubLess ="";
+//        for (int i = 0; i < str.length(); i++) {
+//            if(!dubLess.contains(str.charAt(i)+"")){
+//                dubLess+=str.charAt(i);
+//            }
+//        }
+//        System.out.println(dubLess);
+
+
+        /**Using Collections:*/
+
+        Set<String> dubsRemover = new TreeSet<>();
+        for (int i = 0; i < str.length(); i++) {
+            dubsRemover.add(""+str.charAt(i));
+        }
+        String dubsRemoved = dubsRemover.toString().
+                replace("[","").
+                replace(",","").
+                replace("]","").replace(" ","");
+
+        System.out.println(dubsRemoved);
+
+        input.close();
     }
 }
--- Anas/week4/SameLetters.java
+++ Anas/week4/SameLetters.java
@@ -1,5 +1,7 @@
 package Anas.week4;
 
+import java.util.Scanner;
+
 public class SameLetters {
     public static void main(String[] args) {
 //        String -- Same letters
@@ -8,5 +10,27 @@ public class SameLetters {
 //                Ex: same("abc", "cab"); -> true
 //        same("abc", "abb"); -> false:
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first word: ");
+        String firstWord = input.next();
+        System.out.println("Enter second word: ");
+        String secondWord = input.next();
+
+
+        String holder="";
+        for (int i = 0; i <firstWord.length(); i++) {
+            for (int j = 0; j <secondWord.length() ; j++) {
+                if(firstWord.charAt(i)==secondWord.charAt(j)){
+                    holder+=secondWord.charAt(j);
+                }
+            }
+        }
+
+        if (firstWord.equals(holder)){
+            System.out.println(true);
+        }else {
+            System.out.println(false);
+        }
+        input.close();
     }
 }
