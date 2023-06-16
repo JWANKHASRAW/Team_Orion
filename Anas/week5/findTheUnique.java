7402532dd6845a642a0746a531e372fe11314ac3
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-06-16 13:48:02 -0700

    week5

---
+++ Anas/week5/findTheUnique.java
@@ -0,0 +1,8 @@
+package Anas.week5;
+
+public class findTheUnique {
+//    String -- Find the unique
+//    Write a return method that can find the unique characters from the String
+//    Ex: unique("AAABBBCCCDEF") ==> "DEF";
+
+}
---
+++ Anas/week5/reverse.java
@@ -0,0 +1,7 @@
+package Anas.week5;
+
+public class reverse {
+//    String -- Reverse
+//    Write a return method that can reverse String
+//    Ex: Reverse("ABCD"); ==> DCBA
+}
