83ad6231bfedbcb9f9d9bd3ca473648f9be2e4ac
Anas_Cydeo <anasbenzougagh@gmail.com>
2023-07-05 15:56:19 -0700

    week6

--- Anas/week6/FindMaximum.java
+++ Anas/week6/FindMaximum.java
@@ -2,14 +2,14 @@ package Anas.week6;
 
 public class FindMaximum {
     public static void main(String[] args) {
-        int [] numbers = {1,89,54,78,9,55,1,2};
+        int [] numbers = {-55,1,89,54,78,9,55,1,2};
         System.out.println(
                 max(numbers)
         );
     }
 
     public static int max(int [] numbers){
-        int max =Integer.MIN_VALUE;
+        int max = Integer.MIN_VALUE;
         for (int i = 0; i <numbers.length; i++) {
             if(numbers[i]>max){
                 max=numbers[i];
