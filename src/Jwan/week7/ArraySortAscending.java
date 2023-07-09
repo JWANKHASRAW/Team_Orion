be19ba83da12fdeab242ddf4b6c05b981410da07
Jwan Khasraw <jwan_khasraw@yahoo.com>
2023-07-08 17:24:22 -0700

    Updated ArraySortAscending.java

--- src/Jwan/week7/ArraySortAscending.java
+++ src/Jwan/week7/ArraySortAscending.java
@@ -1,12 +1,21 @@
 package Jwan.week7;
 
+import java.util.Arrays;
+import java.util.TreeSet;
+
 public class ArraySortAscending {
     public static void main(String[] args) {
 
-        int[] arr = {10, 9, 8, 7, -1, -2};
-        for (int i = arr.length - 1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
+        int[] arr = {10, 9, 8, 7};
+        int i=0;
+        Integer[]arr2= new Integer[arr.length];
+        for (int each: arr){
+            arr2[i++]= Integer.valueOf(each);
+
         }
+        TreeSet<Integer> ascendingSet = new TreeSet<>(Arrays.asList(arr2));
+        System.out.println(ascendingSet);
+
 
 
 
