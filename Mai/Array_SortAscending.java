f9b493d1ca0d619120cdbbc5847dfc8bc0f56207
thaladch <moggrand@gmail.com>
2023-07-10 19:32:24 -0400

    created Array Sort Ascending

---
+++ Mai/Array_SortAscending.java
@@ -0,0 +1,18 @@
+package Mai;
+
+import java.util.Arrays;
+
+public class Array_SortAscending {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {10, 9, 8, 7};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+}
